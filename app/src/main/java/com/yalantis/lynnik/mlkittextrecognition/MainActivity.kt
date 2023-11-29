8a6a13f33ada2f65b6c4c362de765abad37c2138
Vlad Lynnik <lynnik.v@gmail.com>
2023-11-29 18:31:11 +0200

    Setup ML Kit Text recognizer

--- app/src/main/java/com/yalantis/lynnik/mlkittextrecognition/MainActivity.kt
+++ app/src/main/java/com/yalantis/lynnik/mlkittextrecognition/MainActivity.kt
@@ -2,10 +2,14 @@ package com.yalantis.lynnik.mlkittextrecognition
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.mlkit.vision.text.TextRecognition
+import com.google.mlkit.vision.text.latin.TextRecognizerOptions
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val recognizer = TextRecognition.getClient(TextRecognizerOptions.DEFAULT_OPTIONS)
     }
 }
