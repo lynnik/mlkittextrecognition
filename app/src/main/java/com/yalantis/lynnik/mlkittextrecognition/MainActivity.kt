59ad8320b7c88cf3be5d97653273d88c7edb91f5
Vlad Lynnik <lynnik.v@gmail.com>
2023-12-01 17:36:04 +0200

    Recognize different parts of a text

--- app/src/main/java/com/yalantis/lynnik/mlkittextrecognition/MainActivity.kt
+++ app/src/main/java/com/yalantis/lynnik/mlkittextrecognition/MainActivity.kt
@@ -5,6 +5,7 @@ import android.graphics.BitmapFactory
 import android.graphics.Canvas
 import android.graphics.Color
 import android.graphics.Paint
+import android.graphics.Rect
 import android.graphics.drawable.BitmapDrawable
 import android.os.Bundle
 import android.widget.Button
@@ -12,64 +13,125 @@ import android.widget.ImageView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.mlkit.vision.common.InputImage
+import com.google.mlkit.vision.text.Text
 import com.google.mlkit.vision.text.TextRecognition
 import com.google.mlkit.vision.text.latin.TextRecognizerOptions
 
 class MainActivity : AppCompatActivity() {
+    private val textRecognizer = TextRecognition.getClient(TextRecognizerOptions.DEFAULT_OPTIONS)
+
+    private var imageView: ImageView? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setupUi()
+        renderImage()
+    }
 
-        val bitmap = BitmapFactory.decodeResource(resources, R.drawable.img_lorem_ipsum)
+    private fun setupUi() {
+        imageView = findViewById(R.id.imageView)
 
-        val recognizer = TextRecognition.getClient(TextRecognizerOptions.DEFAULT_OPTIONS)
-        val image = InputImage.fromBitmap(bitmap, 0)
-        recognizer.process(image)
+        findViewById<Button>(R.id.blocksButton).setOnClickListener {
+            renderRecognizedBlocks()
+        }
+        findViewById<Button>(R.id.linesButton).setOnClickListener {
+            renderRecognizedLines()
+        }
+        findViewById<Button>(R.id.elementsButton).setOnClickListener {
+            renderRecognizedElements()
+        }
+        findViewById<Button>(R.id.symbolsButton).setOnClickListener {
+            renderRecognizedSymbols()
+        }
+        findViewById<Button>(R.id.clearButton).setOnClickListener {
+            renderImage()
+        }
+    }
+
+    private fun provideImage(): Bitmap {
+        return BitmapFactory.decodeResource(resources, R.drawable.img_lorem_ipsum)
+    }
+
+    private fun renderImage() {
+        imageView?.setImageBitmap(provideImage())
+    }
+
+    private fun renderRecognizedBlocks() {
+        val bitmap = provideImage()
+        recognizeText(bitmap) { blocks ->
+            val rectangles = blocks.mapNotNull { it.boundingBox }
+            renderRecognizedRectangles(bitmap, rectangles)
+        }
+    }
+
+    private fun renderRecognizedLines() {
+        val bitmap = provideImage()
+        recognizeText(bitmap) { blocks ->
+            val lines = blocks.map { it.lines }.flatten()
+            val rectangles = lines.mapNotNull { it.boundingBox }
+            renderRecognizedRectangles(bitmap, rectangles)
+        }
+    }
+
+    private fun renderRecognizedElements() {
+        val bitmap = provideImage()
+        recognizeText(bitmap) { blocks ->
+            val lines = blocks.map { it.lines }.flatten()
+            val elements = lines.map { it.elements }.flatten()
+            val rectangles = elements.mapNotNull { it.boundingBox }
+            renderRecognizedRectangles(bitmap, rectangles)
+        }
+    }
+
+    private fun renderRecognizedSymbols() {
+        val bitmap = provideImage()
+        recognizeText(bitmap) { blocks ->
+            val lines = blocks.map { it.lines }.flatten()
+            val elements = lines.map { it.elements }.flatten()
+            val symbols = elements.map { it.symbols }.flatten()
+            val rectangles = symbols.mapNotNull { it.boundingBox }
+            renderRecognizedRectangles(bitmap, rectangles)
+        }
+    }
+
+    private fun recognizeText(
+        bitmap: Bitmap,
+        onRecognizedBlocks: (List<Text.TextBlock>) -> Unit
+    ) {
+        val inputImage = InputImage.fromBitmap(bitmap, 0)
+        textRecognizer.process(inputImage)
             .addOnSuccessListener { visionText ->
-                val blocks = visionText.textBlocks
-                if (blocks.isEmpty()) {
+                if (visionText.textBlocks.isEmpty()) {
                     showToast("No text found")
                     return@addOnSuccessListener
                 }
-
-                val bitmapPaint = Paint()
-                val blocksPaint = Paint().apply {
-                    color = Color.RED
-                    style = Paint.Style.STROKE
-                    strokeWidth = 8f
-                }
-
-                val tempBitmap =
-                    Bitmap.createBitmap(bitmap.width, bitmap.height, Bitmap.Config.ARGB_8888)
-
-                val canvas = Canvas(tempBitmap)
-                canvas.drawBitmap(bitmap, 0f, 0f, bitmapPaint)
-                for (block in blocks) {
-                    block.boundingBox?.let { canvas.drawRect(it, blocksPaint) }
-                }
-
-                val imageView = findViewById<ImageView>(R.id.imageView)
-                imageView.setImageDrawable(BitmapDrawable(resources, tempBitmap))
+                onRecognizedBlocks(visionText.textBlocks)
             }
             .addOnFailureListener { exception ->
                 showToast(exception.localizedMessage)
             }
     }
 
-    private fun setupUi() {
-        findViewById<Button>(R.id.blocksButton).setOnClickListener {
-            // TODO
+    private fun renderRecognizedRectangles(bitmap: Bitmap, rectangles: List<Rect>) {
+        val bitmapPaint = Paint()
+        val rectanglePaint = Paint()
+            .apply {
+                color = Color.RED
+                style = Paint.Style.STROKE
+                strokeWidth = 8f
+            }
+
+        val tempBitmap = Bitmap.createBitmap(bitmap.width, bitmap.height, Bitmap.Config.ARGB_8888)
+
+        val canvas = Canvas(tempBitmap)
+            .apply { drawBitmap(bitmap, 0f, 0f, bitmapPaint) }
+        for (rectangle in rectangles) {
+            canvas.drawRect(rectangle, rectanglePaint)
         }
-        findViewById<Button>(R.id.linesButton).setOnClickListener {
-            // TODO
-        }
-        findViewById<Button>(R.id.elementsButton).setOnClickListener {
-            // TODO
-        }
-        findViewById<Button>(R.id.clearButton).setOnClickListener {
-            // TODO
-        }
+
+        val bitmapDrawable = BitmapDrawable(resources, tempBitmap)
+        imageView?.setImageDrawable(bitmapDrawable)
     }
 
     private fun showToast(text: CharSequence?) {
